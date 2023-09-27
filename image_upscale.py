2d5923c105ee7186f06471b6ae1a168261f5dc26
zain5266 <zaindatalytics@gmail.com>
2023-09-27 11:46:12 +0500

    code uploaded

--- image_upscale.py
+++ image_upscale.py
@@ -29,7 +29,7 @@ def process_upscale(input_path, output_path, model, scale=4):
 def upscale_image(img_path,output_path,model_name,scale):
     # Provide the input image path, output image path, model, and scale
     scale = 4  # Adjust the scale as needed
-    result_path = upscale_image(img_path, output_path, model_name, scale)
+    result_path = process_upscale(img_path, output_path, model_name, scale)
     if result_path:
         print(f"Upscaled image saved at: {result_path}")
         return result_path
